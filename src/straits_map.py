fd6871af6d491d025eec049799f73792d5dfc1d6
Ewan Pinnington <ewan.pinnington@gmail.com>
2015-07-02 15:44:59 +0100

    added new csvs

--- src/straits_map.py
+++ src/straits_map.py
@@ -38,8 +38,10 @@ def latlon2bng(lon,lat):
     bng = pyproj.Proj(init='epsg:27700')
     wgs84 = pyproj.Proj(init='epsg:4326')
 
-    lon,lat = pyproj.transform(wgs84, bng, lon, lat)
-    return lon,lat
+    easting, northing = pyproj.transform(wgs84, bng, lon, lat)
+    easting = round(easting, 0)
+    northing = round(northing, 0)
+    return easting, northing
 
 def latlonlist2bng(latlonl):
     """ Converts list of lat lon values to BNG coords.
@@ -75,11 +77,17 @@ def uniquelist(seq):
     seen_add = seen.add
     return [ x for x in seq if not (x in seen or seen_add(x))]
 
-def save_latlon2file(latlonlist, fname):
+def save_bng2file(bnglist, fname):
     """ Save transect point list to file.
     """
-    latlonlist= np.array(latlonlist)
-    np.savetxt(fname, latlonlist, delimiter=',', header='Easting, Northing', fmt='%1.0f')
+    arr = np.zeros((len(bnglist), 3))
+    for x in xrange(len(arr)):
+        arr[x,0]=bnglist[x][0]
+        arr[x,1]=bnglist[x][1]
+        if bnglist[x] in mensuration_plot_dict.values():
+            arr[x,2]=1
+    np.savetxt(fname, arr, delimiter=',', header='Easting, Northing, M_plot', fmt='%1.0f')
+    return arr
 
 def plot_transect(map, transects, samplength=50):
     fluxtower = (51.153533, -0.8583)
@@ -92,7 +100,7 @@ def plot_transect(map, transects, samplength=50):
             lat1, lon1 = bng2latlon(tsect[x][0],tsect[x][1])
             lat2, lon2 = bng2latlon(tsect[x+1][0],tsect[x+1][1])
             (az12, az21, dist) = g.inv(lon1,lat1,lon2,lat2)
-            lonlats = g.npts(lon1, lat1, lon2, lat2, 1+int(dist/samplength))
+            lonlats = g.npts(lon1, lat1, lon2, lat2, (dist/samplength))
 
             x1, y1 = map(lat1, lon1)
             x2, y2 = map(lat2, lon2)
@@ -149,7 +157,51 @@ def plot_three_t(map, sampdist=50):
     bnglist = latlonlist2bng(lonlatslist)
     return bnglist
 
-transect = [(480436, 139989), (480304, 139935), (480208, 139816), (480087, 139849), (480087,139885),
-               (479957, 139885), (479833, 139917), (479839, 139820), (479867, 139757), (479634, 139716),
-               (479491, 139718), (479500, 139940), (479543, 140051), (479630, 140000), (479640, 140150),
-                ]
+def plot_single_t(map, sampdist=50):
+    key_list = ['E26', 'E4', 'E8', 'E9', 'lakepoint', 'E20', 'W13', 'W11', 'W10', 'W15', 'W7',
+                'W3', 'W18', 'W16', 'E19', 'E11', 'E14', 'E23', 'E5', 'E25']
+    lakepoint = (480087,139885)
+    transect = []
+    for x in key_list:
+        if x=='lakepoint':
+            transect.append(lakepoint)
+        else:
+            transect.append(mensuration_plot_dict[x])
+    t_list = [transect]
+    lonlatslist = plot_transect(map, t_list, sampdist)
+    bnglist = latlonlist2bng(lonlatslist)
+    return bnglist
+
+def plot_mensuration(map):
+    """Plots all mensuration sites.
+    """
+    fluxtower = (51.153533, -0.8583)
+    flux_x, flux_y = map(fluxtower[1], fluxtower[0])
+    map.plot(flux_x, flux_y, marker='D', color='k')
+    for x in xrange(len(mensuration_plot_dict)):
+        lon, lat = bng2latlon(mensuration_plot_dict.values()[x][0], mensuration_plot_dict.values()[x][1])
+        xplt, yplt = map(lon, lat)
+        map.plot(xplt, yplt, marker='x', color='k')
+        plt.scatter(xplt+30, yplt, marker='$ %s $'%mensuration_plot_dict.keys()[x], s=300, color='m')
+    plt.show()
+
+transect = [[(480436, 139989), (480304, 139935), (480208, 139816), (480087, 139849), (480087,139885),
+            (479957, 139885), (479833, 139917), (479839, 139820), (479867, 139757), (479634, 139716),
+            (479491, 139718), (479500, 139940), (479543, 140051), (479630, 140000), (479640, 140150),
+            ]]
+
+mensuration_plot_dict={'W1': (479640, 140150), 'W2': (479630, 140000), 'W3': (479500, 139940), 'W4': (479410, 139760),
+                       'W5': (479250, 139620), 'W6': (479360, 139560), 'W7': (479491, 139718), 'W8': (479569, 139600),
+                       'W9': (479673, 139599), 'W10': (479867, 139757), 'W11': (479839, 139820),
+                       'W12': (479602, 139813), 'W13': (479833, 139892), 'W14': (479768, 139917),
+                       'W15': (479634, 139716), 'W16': (479759, 140087), 'W17': (479416, 140134),
+                       'W18': (479543, 140051),
+                       'E3': (480421, 139852), 'E4': (480304, 139935), 'E5': (480207, 140105), 'E6': (480022, 140098),
+                       'E7': (480225, 139760), 'E8': (480208, 139816), 'E9': (480087, 139849), 'E10': (480115, 139730),
+                       'E11': (480010, 140258), 'E12': (480143, 140376), 'E13': (479982, 140425),
+                       'E14': (480086, 140328), 'E15': (479898, 140343), 'E16': (479792, 140389),
+                       'E17': (479600, 140353), 'E18': (479723, 140285), 'E19': (479869, 140190),
+                       'E20': (479957, 139885), 'E21': (479982, 139782), 'E22': (479999, 139679),
+                       'E23': (480192, 140205), 'E24': (480280, 140144), 'E25': (480360, 140047),
+                       'E26': (480436, 139989)
+                       }
